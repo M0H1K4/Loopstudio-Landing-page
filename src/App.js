652f366c31b742de6857f1b11d2b36e95a20bbea
M0H1K4N <nejer911@gmail.com>
2023-10-24 09:32:27 +0400

    adding mobile media

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Middle from './Components/Middle/Middle'
 import Pictures from './Components/Pictures/Pictures'
 import Footer from './Components/Footer/Footer'
 import Added from './Components/Added/Added'
+import Mobile from './Components/Mobile/Mobile'
 import "./App.css";
 
 function App() {
   return (
   <div className="App">
+    <Mobile/>
     <Header/>
     <Middle/>
     <Added/>
---
+++ src/Components/Mobile/Mobile.js
@@ -0,0 +1,26 @@
+import React from "react";
+import MidImg from '../../images/mobile/image-interactive.jpg'
+
+import "./Mobile.css";
+
+export default function Mobile() {
+  return (
+    <div className="Mobile_div">
+      <header className="Mobile-header">
+        <div className="Mobile-txt_div">
+          <h1 className="Mobile-txt">Immersive experiences that deliver</h1>
+        </div>
+      </header>
+      <div className="middle">
+        <img className="Mobile_img" src={MidImg} />
+        <h1>The leader in interactive VR</h1>
+        <p className="middle-p">
+          Founded in 2011, Loopstudios has been producing world-class virtual
+          reality projects for some of the best companies around the globe. Our
+          award-winning creations have transformed businesses through digital
+          experiences that bind to their brand.
+        </p>
+      </div>
+    </div>
+  );
+}
